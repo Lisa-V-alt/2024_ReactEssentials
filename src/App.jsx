8a92fb3d1ba9d8901a482694ce01eb7b316cccad
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-16 23:20:30 +1000

    added CoreConcept props, simplified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
+//randomised constants for homepage desc.
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 //function for randomising constants in homepage description
@@ -7,22 +10,49 @@ function genRandomInt(max) {
 }
 
 function Header(){
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (<header>
-  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+  <img src={reactImg} alt="Stylized atom" />
   <h1>React Essentials</h1>
   <p>
-    {reactDescriptions [genRandomInt(2)]} React concepts you will need for almost any app you are
+    {description} React concepts you will need for almost any app you are
     going to build!
   </p>
 </header>);
 }
 
+//destructuring to simplify CoreConcept props under App()
+function CoreConcept({image, title, description}) {
+  return (
+  <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+  );
+}
+
+//CoreConcept props imported from data.js:
 function App() {
   return (
     <div>
     <Header/>
       <main>
-        <h2>Time to get started!</h2>
+      <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        <CoreConcept  //longer version (just as an example)
+        title={ CORE_CONCEPTS[0].title }
+        description={ CORE_CONCEPTS[0].description }
+        image={ CORE_CONCEPTS[0].image }
+        />
+        <CoreConcept {...CORE_CONCEPTS[1]}/>
+        <CoreConcept {...CORE_CONCEPTS[2]}/>
+        <CoreConcept {...CORE_CONCEPTS[3]}/>
+      </ul>
+      </section>
+        
       </main>
     </div>
   );
