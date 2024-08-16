6f54d956cb84089a68467237f48885b397b13ffe
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-16 22:20:48 +1000

    inserted randomised reactDescriptions constant on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+//function for randomising constants in homepage description
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header(){
   return (<header>
   <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
   <h1>React Essentials</h1>
   <p>
-    Fundamental React concepts you will need for almost any app you are
+    {reactDescriptions [genRandomInt(2)]} React concepts you will need for almost any app you are
     going to build!
   </p>
 </header>);
@@ -12,7 +20,7 @@ function Header(){
 function App() {
   return (
     <div>
-    <Header></Header>
+    <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
