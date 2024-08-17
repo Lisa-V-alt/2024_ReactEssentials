1dbaa05dc11bd2ff67aa900231a9450ffe26dde1
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-17 16:00:01 +1000

    updated CoreConcept function to adapt dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,11 @@ if (selectedTopic){
       <section id="core-concepts">
       <h2>Core Concepts</h2>
       <ul>
+        {/* this function executes once for every item in the array: */}
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
+        {/* former function structure:
         <CoreConcept  //longer version (just as an example)
         title={ CORE_CONCEPTS[0].title }
         description={ CORE_CONCEPTS[0].description }
@@ -50,7 +55,7 @@ if (selectedTopic){
         />
         <CoreConcept {...CORE_CONCEPTS[1]}/>
         <CoreConcept {...CORE_CONCEPTS[2]}/>
-        <CoreConcept {...CORE_CONCEPTS[3]}/>
+        <CoreConcept {...CORE_CONCEPTS[3]}/> */}
       </ul>
       </section>
       <section id="examples">
